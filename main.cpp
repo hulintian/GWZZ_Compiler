d8ca4551c6e1c3a18ac2de6aa67e1f2bcc027d0c
hulintian <863469703@qq.com>
2025-02-24 09:34:23 +0800

    lexer

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fsstream>
+
+#include <antlr4-runtime/antlr4-runtime.h>
+
+using namespace std;
+using namespace antlr4;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
