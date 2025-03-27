c1bd9f0c696245c774e266c43267f6dee1ac515a
hulintiangit <863469703@qq.com>
2025-03-28 07:40:27 +0800

    3.28

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['ast_2ecpp_0',['AST.cpp',['../AST_8cpp.html',1,'']]],
+  ['ast_2ehpp_1',['AST.hpp',['../AST_8hpp.html',1,'']]],
+  ['astvisitor_2',['ASTVisitor',['../classASTVisitor.html',1,'']]],
+  ['astvisitor_2ecpp_3',['ASTVisitor.cpp',['../ASTVisitor_8cpp.html',1,'']]],
+  ['astvisitor_2eh_4',['ASTVisitor.h',['../ASTVisitor_8h.html',1,'']]]
+];
---
+++ notes/ir.md
@@ -0,0 +1,14 @@
+# IR 
+
+* In namespace middleend::ir
+```mermaid
+classDiagram
+class Value 
+
+Value <|-- BasicBlock 
+Value <|-- Function 
+Value <|-- Instruction 
+
+Value <|-- User
+
+```
