60d9ab498acdda1a57af1e037f35222d6f3c8c31
hulintiangit <863469703@qq.com>
2025-07-06 14:53:59 +0800

    write templates to generate code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include "IR/IR.hpp"
-#include "frontend/emit_ir.hpp"
 #include "grammar/Sysy22Lexer.h"
 #include "grammar/Sysy22Parser.h"
 #include "grammar/Sysy22Visitor.h"
@@ -14,9 +12,6 @@
 #include <frontend/ASTVisitor.h>
 #include <ostream>
 #include "frontend/Sema.hpp"
-#include "IR/IR.hpp"
-#include "backend/program.hpp"
-// #include "backend/passes.hpp"
 
 #include <fstream>
 
@@ -33,37 +28,15 @@ int main(int argc, char** argv) {
     Sysy22Parser parser(&tokens);
 
     auto tree = parser.compUnits();
-//     cout << tree->toStringTree() << endl;
-//    auto tree = parser.prog();
+
     frontend::ASTVisitor av;
     av.visit(tree);
     auto &cu = av.compUnit();
-    cu.print(cout, 0);
+    // cu.print(cout, 0);
 
     frontend::Sema sema;
     sema.visit_compUnits(cu);
-
-    frontend::GenIr *genIr = new frontend::GenIr();
-    midend::ir::Module *module = genIr->transform(cu);
-
-    module->print(cout);
-
-    // get file name 
-    const char* dot = std::strchr(input_path, '.');
-    std::string before_dot;
-    if(dot) {
-        before_dot = std::string(input_path, dot);
-    }
-
-    std::ofstream out(before_dot+".s");
-
-    backend::riscv::Program *prog = new backend::riscv::Program(module);
-    // backend::riscv::backend_passes(prog);
-
-    cout << endl;
-    prog->gen_asm(cout);
-    prog->gen_asm(out);
-    out.close();
+    cu.print(cout, 0);
 
     return 0;
 }
