beed6eb42264340ece31866d77fd58c497d634e4
hulintiangit <863469703@qq.com>
2025-07-07 18:26:18 +0800

    write templates to generate code

--- src/IR/BasicBlock.hpp
+++ src/IR/BasicBlock.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <string>
 
 namespace IR {
@@ -7,8 +8,16 @@ namespace IR {
 class Function;
 class BasicBlock{
 public:
-    BasicBlock(const std::string &name, Function* func) : _parent(func), bb_label(name) {}
+    BasicBlock(const std::string &name, Function* func, int idx) : _parent(func), bb_label(name), _idx(idx) {}
 
+    BasicBlock(const std::string &name,  int idx) : bb_label(name), _idx(idx) {}
+    
+    void set_parent(Function* func) {
+        assert(func && "The function ptr is null\n");
+        this->_parent = func;
+    }
+
+    int get_bb_idx() const { return _idx; }
 private:
     Function* _parent;
     std::string bb_label;
--- src/IR/Value.hpp
+++ src/IR/Value.hpp
@@ -17,6 +17,7 @@ public:
     Value(Type *t, const std::string &name) : _type(t), _name(name) {}
     ~Value() = default;
     Type *get_type() const;
+    std::string get_name() { return _name; }
     std::list<Use> &get_use_list() { return _use_list; }
     void add_use(Value *val, unsigned arg_no = 0) { 
         Use u = {val, arg_no};
