19a19714536cf967abed565db5f99860b1b924d5
hulintiangit <863469703@qq.com>
2025-07-09 15:01:13 +0800

    cg

--- src/IR/BasicBlock.cpp
+++ src/IR/BasicBlock.cpp
@@ -1,11 +1,12 @@
 #include "BasicBlock.hpp"
+#include "IR/Instructions.hpp"
 #include <string>
 #include "common/utils.hpp"
 
 namespace IR {
 
 void BasicBlock::dump(std::ostream& out) {
-    out << this->bb_label << std::to_string(this->get_bb_idx());
+    out << this->bb_label << std::to_string(this->get_bb_idx()) << "\n";
     for(auto i : this->get_intrs()) {
          print_indent(out, 4);
          out << i->to_str() 
