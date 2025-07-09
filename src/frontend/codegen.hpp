0601fa19d8be1fd67fab07ad58f88864e44cb422
hulintiangit <863469703@qq.com>
2025-07-09 18:49:14 +0800

    cg

--- src/frontend/codegen.hpp
+++ src/frontend/codegen.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <IR/IRBuilder.hpp>
 
+#include "IR/BasicBlock.hpp"
 #include "IR/Context.hpp"
 #include "IR/Function.hpp"
 #include "IR/GlobalValue.hpp"
@@ -21,17 +22,27 @@ public:
     void gen_gv(const ast::Decl& decl);
     IR::Function* gen_func(const ast::Func& func);
 
+    void gen_func_body(const ast::Block& block);
+
+    void gen_block(const ast::Block& block);
+    void gen_stmt(const ast::Stmt& stmt);
+    void gen_decl(const ast::Decl& decl);
+
+    IR::Instruction* gen_expr(const ast::Expr& expr);
+
     // this is for 
     // void gen_decl(const ast::Decl& decl);
     // void gen_stmt(const ast::Stmt& stmt);
     // void gen_while(const ast::WhileStmt& while_stmt);
     // void gen_if(const ast::IfStmt& if_stmt);
 
-    IR::Function* cur_func;
-    IR::BasicBlock* cur_bb;
     IR::Module* _module;
     IR::IRBuilder* builder;
     Context *ctx;
+
+    IR::Function* get_cur_func() { return ctx->get_current_function(); }
+    IR::BasicBlock* get_cur_bb() { return ctx->get_current_basic_block(); }
+    IR::Module* get_cur_module() { return ctx->get_current_module(); }
 };
 
 }
