a8a3cbbb628873e37703e9bc47ebd3b1c49b1504
hulintiangit <863469703@qq.com>
2025-07-09 18:49:14 +0800

    cg

--- src/IR/GlobalValue.cpp
+++ src/IR/GlobalValue.cpp
@@ -6,8 +6,8 @@ namespace IR {
 
 void GlobalValue::dump(std::ostream& out) {
     out << "@" << this->get_symbol() 
-        << " global" 
-        << type_string(this->get_type()) 
+        << " = global " 
+        << type_string(this->get_type()) << " "
         << this->get_var() 
         << "\n";
 }
