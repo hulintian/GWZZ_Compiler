ecbd2c6a8e8aa0640b05a4b2a1851cd77a0a889d
hulintiangit <863469703@qq.com>
2025-07-09 18:49:14 +0800

    cg

--- src/frontend/codegen.cpp
+++ src/frontend/codegen.cpp
@@ -1,6 +1,7 @@
 #include "frontend/codegen.hpp"
 #include "IR/Function.hpp"
 #include "IR/GlobalValue.hpp"
+#include "IR/Instructions.hpp"
 #include "IR/Module.hpp"
 #include "common/type.hpp"
 #include "frontend/AST.hpp"
@@ -56,12 +57,43 @@ IR::Function* CodeGen::gen_func(const ast::Func& func) {
 
     
     // then translate the body 
+    ctx->set_current_function(nf);
+    auto &func_body = func.body();
+    gen_func_body(*func_body);
 
     
     // set the insert ptr nullptr, exit func
     ctx->set_current_basic_block(nullptr);
-    ctx->set_current_function(nf);
+    ctx->set_current_function(nullptr);
     return nf;
+}
+
+void CodeGen::gen_func_body(const ast::Block& block) {
+    assert(this->get_cur_func() != nullptr && "not in function context\n");
+    auto &children = block.children();
+    for(auto &child : children) {
+        if(child.index() == 0) {
+            auto &stmt = std::get<std::unique_ptr<ast::Stmt>>(child);
+            gen_stmt(*stmt);
+        } else if(child.index() == 1) {
+            auto &decl = std::get<std::unique_ptr<ast::Decl>>(child);
+            gen_decl(*decl);
+        }
+    }
+}
+
+void CodeGen::gen_decl(const ast::Decl& decl) {
+// 对于重复定义的要建一个别名表
+    // gv中找，check exists same symbol in 
+}
+
+void CodeGen::gen_stmt(const ast::Stmt& stmt) {
+
+}
+
+IR::Instruction* gen_expr(const ast::Expr& expr) {
+
+    return nullptr;
 }
 
 }
