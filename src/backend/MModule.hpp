e5e2f3c3b605a9f40febc632e9b6a6af99e54a51
hulintiangit <863469703@qq.com>
2025-07-22 19:55:38 +0800

    fix if branch jump

--- src/backend/MModule.hpp
+++ src/backend/MModule.hpp
@@ -1,5 +1,6 @@
 #pragma once 
 
+#include "IR/GlobalValue.hpp"
 #include <backend/MBasicBlock.hpp>
 #include <backend/MFunction.hpp>
 #include <vector>
@@ -7,8 +8,9 @@ namespace backend {
 
 class MachineModule {
 public:
+    std::string _mname;
     std::vector<MachineFunction*> funcs;
-
+    std::vector<IR::GlobalValue*> global_items;
 };
 
 }
