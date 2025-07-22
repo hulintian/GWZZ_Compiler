d9662f699a4e77b9cee0e6426826ae3d34f94c2c
hulintiangit <863469703@qq.com>
2025-07-22 19:55:38 +0800

    fix if branch jump

--- src/backend/MBasicBlock.hpp
+++ src/backend/MBasicBlock.hpp
@@ -1,8 +1,13 @@
 #pragma once 
 
+#include "backend/MInstruction.hpp"
+#include <vector>
 namespace backend {
 
 class MachineBasicBlock {
+
+    int _bb_idx;
+    std::vector<MachineInstr*> m_instrs;
 };
 
 }
