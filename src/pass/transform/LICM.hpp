7d233beb388e6237627b5fa91db05d4a2be8de6d
SasaRaaa <ongaurlau@163.com>
2025-07-24 13:01:04 -0700

    add LICM and LoopInfo pass

---
+++ src/pass/transform/LICM.hpp
@@ -0,0 +1,29 @@
+#include "pass/Pass.hpp"
+#include <set>
+namespace IR{
+class BasicBlock;
+class Instruction;
+}
+namespace pass{
+class Loop;
+class DominatorTreeResult;
+class AliasAnalysisResult;
+
+class LICMPass : public FunctionTransformPass {
+public:
+    const char* get_name()const override {return "LoopInvariantCodeMotion";}
+    bool run(IR::Function& function,PassManager& pm) override;
+
+private:
+    using LoopInvariantsSet = std::set<IR::Instruction *>; 
+    bool run_on_loop (Loop* loop,const DominatorTreeResult& dom_tree,AliasAnalysisResult& AA);
+    bool is_loop_invariant (IR::Instruction* inst, 
+                            const Loop* loop, 
+                            const LoopInvariantsSet& hoisted_insts);
+    bool can_be_safely_hoisted (IR::Instruction* inst, 
+                                const Loop* loop,
+                                const DominatorTreeResult& dom_tree,
+                                AliasAnalysisResult& AA);
+    IR::BasicBlock* get_or_create_preheader(Loop* loop, const DominatorTreeResult& dom_tree);
+};
+}
