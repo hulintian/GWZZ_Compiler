cec3c17f1e538378cd8efb2bb44c7297cf12f0fa
hulintiangit <863469703@qq.com>
2025-07-26 21:13:42 +0800

    test asm j instr

---
+++ src/backend/MFunction.cpp
@@ -0,0 +1,30 @@
+#include "backend/MFunction.hpp"
+#include "backend/MBasicBlock.hpp"
+#include <cassert>
+#include <ostream>
+
+namespace backend {
+
+void MachineFunction::add_mbb(MachineBasicBlock* mbb) {
+    assert(!this->has_mbb(mbb->get_m_bb_idx()));
+    this->bbs.insert(mbb);
+    idx_bb_mp[mbb->get_m_bb_idx()] = mbb;
+}
+
+MachineBasicBlock* MachineFunction::get_mbb(int idx) {
+    assert(has_mbb(idx) && "Not has machin basic block");
+    return this->idx_bb_mp[idx];
+}
+
+void MachineFunction::dump_asm(std::ostream& out) {
+    out << this->_name << ": \n";
+    this->prologue_bb->dump_asm(out);
+    for(auto bb: bbs) {
+        bb->dump_asm(out);
+        out << "\n";
+    }
+    this->epilogue_bb->dump_asm(out);
+}
+
+}
+
