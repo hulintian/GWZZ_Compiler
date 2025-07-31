0564f68504dd9894918f5bf7e721533901ca9c5c
hulintiangit <863469703@qq.com>
2025-07-31 20:32:59 +0800

    fix fp cmp br and ir unary op

--- src/frontend/codegen.cpp
+++ src/frontend/codegen.cpp
@@ -449,9 +449,9 @@ Value* CodeGen::gen_expr(const ast::Expr* expr) {
         auto zero = new ConstValue(0);
         switch (uop) {
             case UnaryOp::Add: return gen_expr(*od) ; break;
-            case UnaryOp::Sub:  builder->create_sub(builder->create_const_value(builder->get_base_type(Int), *zero), this->gen_expr(*od)) ; break;
+            case UnaryOp::Sub: return builder->create_sub(builder->create_const_value(builder->get_base_type(Int), *zero), this->gen_expr(*od)) ; break;
             // wait to impl when deal with cond expr, cmp with 0, this should return i1 type
-            case UnaryOp::Not:  builder->create_ne(builder->create_const_value(builder->get_base_type(Int), *zero), this->gen_expr(*od)) ; break;
+            case UnaryOp::Not: return  builder->create_ne(builder->create_const_value(builder->get_base_type(Int), *zero), this->gen_expr(*od)) ; break;
         }
         return nullptr; // should never go here
     } else if( auto call = dynamic_cast<const ast::Call*>(expr)) {
