21bb227d7b7a2ad2690b454ed4ce223529fcc3e6
SasaRaaa <ongaurlau@163.com>
2025-08-19 17:20:06 -0700

    add sth

--- src/IR/opt.cpp
+++ src/IR/opt.cpp
@@ -44,7 +44,7 @@ namespace opt
         // pm.add_function_transform_pass(std::make_unique<pass::DCEPass>());
         pm.add_function_transform_pass(std::make_unique<pass::CFGSimplifyPass>());
 
-        pm.add_function_transform_pass(std::make_unique<pass::PHIEliminatePass>());
+        //pm.add_function_transform_pass(std::make_unique<pass::PHIEliminatePass>());
         //pm.add_function_transform_pass(std::make_unique<pass::PHISimplifyPass>());
         //pm.add_function_transform_pass(std::make_unique<pass::AliasTestPass>());
         //pm.add_function_transform_pass(std::make_unique<pass::LoopInfoPrinterPass>());
